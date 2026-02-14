833482ff603fb9a2459f59630052f23d1525affe
Luis Otavio Bertolucci Oliveira <devluisbertolucci@gmail.com>
2026-02-13 23:34:05 -0300

    Optimize server initialization with caching
    Refactor server initialization to cache the app instance for performance.

--- vercel/main.ts
+++ vercel/main.ts
@@ -2,18 +2,27 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '../src/modules/app/app.module'
 import { ExpressAdapter } from '@nestjs/platform-express'
 import express from 'express'
-import serverless from 'serverless-http'
 
-const server = express()
+let cachedApp;
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  )
-  await app.init()
+  if (!cachedApp) {
+    const server = express()
+
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+    )
+
+    await app.init()
+
+    cachedApp = server
+  }
+
+  return cachedApp
 }
 
-bootstrap()
-
-export default serverless(server);
+export default async function handler(req, res) {
+  const server = await bootstrap()
+  return server(req, res)
+}
