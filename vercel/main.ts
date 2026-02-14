5eeffc687adeae51bd3da386e23e5b07794e653a
Lanza <lanzadev1@gmail.com>
2026-02-14 13:18:55 -0300

    Update APP_NAME in .env and enhance main.ts with Swagger documentation setup

--- vercel/main.ts
+++ vercel/main.ts
@@ -1,28 +1,43 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from '../src/modules/app/app.module'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import express from 'express'
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import express from 'express';
+import { AppModule } from '../src/modules/app/app.module';
 
 let cachedApp;
 
 async function bootstrap() {
   if (!cachedApp) {
-    const server = express()
+    const server = express();
 
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(server),
-    )
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-    await app.init()
+    const config = new DocumentBuilder()
+      .setTitle(process.env.APP_NAME!)
+      .setDescription('API Documentation')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+        'access-token',
+      )
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
 
-    cachedApp = server
+    SwaggerModule.setup('docs', app, documentFactory);
+
+    await app.init();
+
+    cachedApp = server;
   }
 
-  return cachedApp
+  return cachedApp;
 }
 
 export default async function handler(req, res) {
-  const server = await bootstrap()
-  return server(req, res)
+  const server = await bootstrap();
+  return server(req, res);
 }
