c75508b0b526fc612effd15414b3f0409d9b6411
Lanza <lanzadev1@gmail.com>
2026-02-13 22:24:15 -0300

    dummy

---
+++ vercel/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/modules/app/app.module';
+import express from 'express';
+import serverless from 'serverless-http';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  await app.init();
+}
+
+void bootstrap();
+
+export const handler = serverless(server);
